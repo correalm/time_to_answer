905a6642f2e0ba761a75027929ac953e111c65c3
miguel <luismigueldasilva.99@gmail.com>
2022-07-03 11:08:45 -0300

    models to nested attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :subject
+  has_many :answers
+  # ao realizar o cadastro, questões devem aceitar atributos aninhados do model answer
+  accepts_nested_attributes_for :answers
 end
