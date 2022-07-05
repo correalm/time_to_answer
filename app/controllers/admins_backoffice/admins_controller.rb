cd6f51022c4c6c8ba86ed3c4afa083c30bb5b9e6
miguel <luismigueldasilva.99@gmail.com>
2022-07-05 20:33:16 -0300

    step 5 ok - Mailer

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -25,6 +25,8 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
+      # Aqui configuro o envio do email -> Cuida deliver_now (para a execução do código nesse momento)
+      AdminMailer.update_email(current_admin, @admin).deliver_now
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
     else
       render :edit
