5e9ed3510c66494789037aa8e0cd64dc728b98f0
miguel <luismigueldasilva.99@gmail.com>
2022-07-05 14:24:36 -0300

    image add with Active Storage

--- app/controllers/users_backoffice_controller.rb
+++ app/controllers/users_backoffice_controller.rb
@@ -1,5 +1,10 @@
 class UsersBackofficeController < ApplicationController
-    layout 'users_backoffice'
-    before_action :authenticate_user!
-
+  layout 'users_backoffice'
+  before_action :build_profile
+  before_action :authenticate_user!
+  
+  private
+  def build_profile
+    current_user.build_user_profile if current_user.user_profile.blank? 
+  end
 end
--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,7 @@
 module UsersBackofficeHelper
+  def img_url
+    image = current_user.user_profile.image
+    image.attached? ? image : 'img.jpg'
+  end
+
 end
